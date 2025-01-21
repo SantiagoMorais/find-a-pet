3cb9b872a5c3ab0d07d03105ca821dc271ade4cd
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-21 12:25:51 -0300

    feat(get-many-by-city): add optional filter parameters for pet search

--- src/core/pet/get-many-by-city-use-case.ts
+++ src/core/pet/get-many-by-city-use-case.ts
@@ -4,6 +4,16 @@ import { z } from "zod";
 const getManyByCitySchema = z.object({
   city: z.string(),
   state: z.string(),
+  filter: z
+    .object({
+      size: z.enum(["SMALL", "MEDIUM", "LARGE"]).optional(),
+      age: z.enum(["PUPPY", "ADULT", "SENIOR"]).optional(),
+      specie: z.enum(["DOG", "CAT", "BIRD", "OTHER"]).optional(),
+      energyLevel: z.number().min(1).max(5).optional(),
+      independencyLevel: z.number().min(1).max(3).optional(),
+      spaceRequirement: z.number().min(1).max(3).optional(),
+    })
+    .optional(),
 });
 
 export type TGetManyByCityRequest = z.infer<typeof getManyByCitySchema>;
