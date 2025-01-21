b3321167899482a1bc3f83174915bb5c2293c04d
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-21 08:53:05 -0300

    fix(env-schema): coerce PORT to number type in environment configuration

--- src/env/index.ts
+++ src/env/index.ts
@@ -2,9 +2,9 @@ import { z } from "zod";
 import "dotenv/config";
 
 const envSchema = z.object({
-  PORT: z.number().default(3000),
-  DATABASE_URL: z.string(),
   NODE_ENV: z.string().default("development"),
+  PORT: z.coerce.number().default(3000),
+  DATABASE_URL: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
