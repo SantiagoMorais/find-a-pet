ea8e56074c2d90135b224ee0b231da7073a89d03
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-22 08:22:07 -0300

    feat(app): add custom error handler for schema validation and internal server errors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,24 @@
 import fastify from "fastify";
 import {
+  hasZodFastifySchemaValidationErrors,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
+import { env } from "./env/index.ts";
 
 export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.setErrorHandler((error, _, res) => {
+  if (hasZodFastifySchemaValidationErrors(error))
+    return res
+      .status(400)
+      .send({ message: "Validation Error", issues: error.message });
+
+  if (env.NODE_ENV !== "production") console.log(error);
+
+  return res.status(500).send({message: "❌💻 Internal server error."})
+});
