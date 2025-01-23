6ebdcabebeabc6223b61ec4a3ece3cbb73ecb9d5
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-23 17:50:36 -0300

    feat(organizations): implement organization profile retrieval use case

---
+++ src/use-cases/organizations/profile.ts
@@ -0,0 +1,16 @@
+import { OrganizationsRepository } from "@/repositories/organizations-repository.ts";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error.ts";
+
+export class OrganizationProfile {
+  constructor(private organizationsRepository: OrganizationsRepository) {}
+
+  async execute(organizationId: string) {
+    const organization =
+      await this.organizationsRepository.findById(organizationId);
+
+    if (!organization) throw new ResourceNotFoundError();
+
+    const { id, created_at, password_hash, ...rest } = organization;
+    return { organization: rest };
+  }
+}
