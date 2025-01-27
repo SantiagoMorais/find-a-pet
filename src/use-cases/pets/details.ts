3a71565424aa738357f4b06b93a70aabaf65913f
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-27 10:29:46 -0300

    fix(pets): throw ResourceNotFoundError if pet is not found in details use case

--- src/use-cases/pets/details.ts
+++ src/use-cases/pets/details.ts
@@ -1,10 +1,13 @@
 import { PetsRepository } from "@/repositories/pets-repository.ts";
+import { ResourceNotFoundError } from "../errors/resource-not-found-error.ts";
 
 export class DetailsUseCase {
   constructor(private petsRepository: PetsRepository) {}
 
   async execute(id: string) {
     const pet = await this.petsRepository.filterById(id);
+
+    if (!pet) throw new ResourceNotFoundError("Pet not found.");
 
     return { pet };
   }
