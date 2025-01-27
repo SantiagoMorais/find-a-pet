a04d42a5deb890f063ded6863ad06bd4fa52cd75
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-27 10:31:14 -0300

    fix(pets): make search pets filter query parameters optional

--- src/core/pet/search-pets-filter-query.ts
+++ src/core/pet/search-pets-filter-query.ts
@@ -1,22 +1,22 @@
 import { z } from "zod";
 
 export const searchPetsFilterQuerySchema = z.object({
-  specie: z.enum(["DOG", "CAT", "BIRD", "OTHER"]),
-  age: z.enum(["PUPPY", "ADULT", "SENIOR"]),
-  size: z.enum(["SMALL", "MEDIUM", "LARGE"]),
+  specie: z.enum(["DOG", "CAT", "BIRD", "OTHER"]).optional(),
+  age: z.enum(["PUPPY", "ADULT", "SENIOR"]).optional(),
+  size: z.enum(["SMALL", "MEDIUM", "LARGE"]).optional(),
   energyLevel: z.coerce.number().refine((value) => value >= 1 && value <= 5, {
     message: "The energy level must be between 1 and 5",
-  }),
+  }).optional(),
   independencyLevel: z.coerce
     .number()
     .refine((value) => value >= 1 && value <= 3, {
       message: "The independency level must be between 1 and 3",
-    }),
+    }).optional(),
   spaceRequirement: z.coerce
     .number()
     .refine((value) => value >= 1 && value <= 3, {
       message: "The space requirement must be between 1 and 3",
-    }),
+    }).optional(),
 });
 
 export type TSearchPetsFilterQuery = z.infer<
